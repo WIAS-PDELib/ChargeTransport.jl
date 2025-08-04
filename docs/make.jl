f98e44eb5e27af1703b5a4fd83594c406b40476d
abdel <abdel@wias-berlin.de>
2025-08-04 15:39:25 +0200

    tried to add pluto notebook to docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,9 @@
-using Documenter, ExampleJuggler, Literate, ChargeTransport, PlutoSliderServer, VoronoiFVM
+using Documenter, ExampleJuggler, Literate, ChargeTransport, PlutoStaticHTML, PlutoSliderServer, VoronoiFVM
 
 DocMeta.setdocmeta!(ExampleJuggler, :DocTestSetup, :(using ExampleJuggler); recursive = true)
 
 exampledir = joinpath(@__DIR__, "..", "examples")
-
+notebookdir = joinpath(@__DIR__, "..", "notebooks")
 
 modules = modules = [
     "Ex101_PIN.jl",
@@ -19,45 +19,17 @@ modules = modules = [
     "Ex202_Laser_simple.jl",
 ]
 
+module_examples = @docmodules(exampledir, modules, use_module_titles = true)
+
 cleanexamples()
 
-module_examples = @docmodules(exampledir, modules, use_module_titles = true)
 #############################################################################
 
 notebooks = [
     "PSC example" => "PSC_example.jl",
 ]
 
-notebookjl = last.(notebooks)
-notebookmd = []
-
-# Use sliderserver to generate html
-notebook_html_dir = joinpath(@__DIR__, "src", "nbhtml")
-export_directory(
-    joinpath(@__DIR__, "..", "pluto-examples"),
-    notebook_paths = notebookjl,
-    Export_output_dir = joinpath(notebook_html_dir),
-    Export_offer_binder = false,
-)
-# generate frame markdown for each notebook
-for notebook in notebookjl
-    base = split(notebook, ".")[1]
-    mdstring = """
-    ##### [$(base).jl](@id $(base))
-    [Download](https://github.com/WIAS-PDELib/ChargeTransport.jl/blob/master/pluto-examples/$(notebook))
-    this [Pluto.jl](https://github.com/fonsp/Pluto.jl) notebook.
-    ```@raw html
-    <iframe style="height:20000px" width="100%" src="../$(base).html"> </iframe>
-    ```
-    """
-    mdname = base * ".md"
-    push!(notebookmd, joinpath("nbhtml", mdname))
-    io = open(joinpath(notebook_html_dir, mdname), "w")
-    write(io, mdstring)
-    close(io)
-end
-
-notebooks = first.(notebooks) .=> notebookmd
+#pluto_examples = @docplutonotebooks(notebookdir, notebooks, iframe = false)
 
 #############################################################################
 
@@ -65,11 +37,11 @@ makedocs(
     sitename = "ChargeTransport.jl",
     modules = [ChargeTransport],
     clean = false,
-    doctest = false,
+    doctest = true,
     authors = "D. Abdel, Z. Amer, P. Farrell, J. Fuhrmann, P. Jaap",
     repo = "https://github.com/WIAS-PDELib/ChargeTransport.jl",
-    pages = Any[
-        "ChargeTransport.jl -- Simulating charge transport in semiconductors" => "general.md",
+    pages = [
+        "Home" => "general.md",
         "Changelog" => "changes.md",
         "Mathematical drift-diffusion models" => "backgroundinfo.md",
         "How to get started" => [
@@ -78,7 +50,7 @@ makedocs(
             "PSC.md",
         ],
         "Types, Constructors and Methods" => "allindex.md",
-        #"Pluto Notebooks" => notebooks,
+        #"Pluto Notebooks" => pluto_examples,
         "Examples" => module_examples,
     ]
 )
