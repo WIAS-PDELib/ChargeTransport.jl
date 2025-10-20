7e4f0aa1cbb7d1144c60ae9039c272d083556915
Documenter.jl <documenter@juliadocs.github.io>
2025-10-20 13:26:32 +0000

    build based on 1f836a2

---
+++ v1.2.4/module_examples/Ex106_PSC_SurfaceRecombination.jl
@@ -0,0 +1,276 @@
+#=
+# PSC device with surface recombination (1D).
+([source code](@__SOURCE_URL__))
+
+Simulating a three layer PSC device PCBM | MAPI | Pedot with mobile ions with a linear scan protocol.
+
+Here, the surface recombination at internal boundaries is tested.
+
+=#
+
+module Ex106_PSC_SurfaceRecombination
+
+using ChargeTransport
+using ExtendableGrids
+using PyPlot
+
+# you can also use other Plotters, if you add them to the example file
+function main(;
+        n = 6, Plotter = PyPlot, plotting = false,
+        verbose = false, test = false,
+        parameter_set = Params_PSC_PCBM_MAPI_Pedot, # choose the parameter set
+        vacancyEnergyCalculation = false,           # assume the vacancy energy level is either given or not
+    )
+
+    if plotting
+        Plotter.close("all")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define physical parameters and model")
+    end
+    ################################################################################
+
+    @local_unitfactors μm cm s ns V K ps Hz W
+
+    # parameter
+    p = parameter_set()
+
+    ## contact voltage
+    voltageAcceptor = 1.2 * V
+
+    ## primary data for I-V scan protocol
+    scanrate = 1.0 * V / s
+    ntsteps = 31
+    vend = voltageAcceptor # bias goes until the given voltage at acceptor boundary
+    tend = vend / scanrate
+
+    ## with fixed timestep sizes we can calculate the times a priori
+    tvalues = range(0, stop = tend, length = ntsteps)
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Set up grid and regions")
+    end
+    ################################################################################
+
+    δ = 4 * n        # the larger, the finer the mesh
+    t = 0.5 * (cm) / δ # tolerance for geomspace and glue (with factor 10)
+    k = 1.5        # the closer to 1, the closer to the boundary geomspace
+
+    coord_n_u = collect(range(0.0, p.h_ndoping / 2, step = p.h_ndoping / (0.8 * δ)))
+    coord_n_g = geomspace(
+        p.h_ndoping / 2, p.h_ndoping,
+        p.h_ndoping / (0.7 * δ), p.h_ndoping / (1.1 * δ),
+        tol = t
+    )
+    coord_i_g1 = geomspace(
+        p.h_ndoping, p.h_ndoping + p.h_intrinsic / k,
+        p.h_intrinsic / (5.1 * δ), p.h_intrinsic / (1.1 * δ),
+        tol = t
+    )
+    coord_i_g2 = geomspace(
+        p.h_ndoping + p.h_intrinsic / k, p.h_ndoping + p.h_intrinsic,
+        p.h_intrinsic / (1.1 * δ), p.h_intrinsic / (5.1 * δ),
+        tol = t
+    )
+    coord_p_g = geomspace(
+        p.h_ndoping + p.h_intrinsic, p.h_ndoping + p.h_intrinsic + p.h_pdoping / 2,
+        p.h_pdoping / (1.3 * δ), p.h_pdoping / (0.6 * δ),
+        tol = t
+    )
+    coord_p_u = collect(range(p.h_ndoping + p.h_intrinsic + p.h_pdoping / 2, p.h_ndoping + p.h_intrinsic + p.h_pdoping, step = p.h_pdoping / (0.8 * δ)))
+
+    coord = glue(coord_n_u, coord_n_g, tol = 10 * t)
+    coord = glue(coord, coord_i_g1, tol = 10 * t)
+    coord = glue(coord, coord_i_g2, tol = 10 * t)
+    coord = glue(coord, coord_p_g, tol = 10 * t)
+    coord = glue(coord, coord_p_u, tol = 10 * t)
+    grid = ExtendableGrids.simplexgrid(coord)
+
+    ## set different regions in grid
+    cellmask!(grid, [0.0 * μm], [p.heightLayers[1]], p.regionDonor, tol = 1.0e-18)     # n-doped region   = 1
+    cellmask!(grid, [p.heightLayers[1]], [p.heightLayers[2]], p.regionIntrinsic, tol = 1.0e-18) # intrinsic region = 2
+    cellmask!(grid, [p.heightLayers[2]], [p.heightLayers[3]], p.regionAcceptor, tol = 1.0e-18)  # p-doped region   = 3
+
+    ## bfacemask! for setting different boundary regions
+    bfacemask!(grid, [0.0], [0.0], p.bregionDonor, tol = 1.0e-18)     # outer left boundary
+    bfacemask!(grid, [p.h_total], [p.h_total], p.bregionAcceptor, tol = 1.0e-18)  # outer right boundary
+    bfacemask!(grid, [p.heightLayers[1]], [p.heightLayers[1]], p.bregionJ1, tol = 1.0e-18) # first  inner interface
+    bfacemask!(grid, [p.heightLayers[2]], [p.heightLayers[2]], p.bregionJ2, tol = 1.0e-18) # second inner interface
+
+    if plotting
+        gridplot(grid, Plotter = Plotter, legend = :lt)
+        Plotter.title("Grid")
+    end
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define System and fill in information about model")
+    end
+    ################################################################################
+
+    ## Initialize Data instance and fill in data
+    data = Data(grid, p.numberOfCarriers)
+
+    ## Possible choices: Stationary, Transient
+    data.modelType = Transient
+
+    ## Possible choices: Boltzmann, FermiDiracOneHalfBednarczyk, FermiDiracOneHalfTeSCA,
+    ## FermiDiracMinusOne, Blakemore
+    data.F = [FermiDiracOneHalfTeSCA, FermiDiracOneHalfTeSCA, FermiDiracMinusOne]
+
+    data.bulkRecombination = set_bulk_recombination(;
+        iphin = p.iphin, iphip = p.iphip,
+        bulk_recomb_Auger = false,
+        bulk_recomb_radiative = true,
+        bulk_recomb_SRH = true
+    )
+
+    ## Possible choices: OhmicContact, SchottkyContact (outer boundary) and InterfaceNone,
+    ## InterfaceRecombination (inner boundary).
+    data.boundaryType[p.bregionAcceptor] = OhmicContact
+    data.boundaryType[p.bregionJ1] = InterfaceRecombination
+    data.boundaryType[p.bregionJ2] = InterfaceRecombination
+    data.boundaryType[p.bregionDonor] = OhmicContact
+
+    ## Present ionic vacancies in perovskite layer
+    enable_ionic_carrier!(data, ionicCarrier = p.iphia, regions = [p.regionIntrinsic])
+
+    ## Choose flux discretization scheme: ScharfetterGummel, ScharfetterGummelGraded,
+    ## ExcessChemicalPotential, ExcessChemicalPotentialGraded, DiffusionEnhanced, GeneralizedSG
+    data.fluxApproximation .= ExcessChemicalPotential
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define Params and fill in physical parameters")
+    end
+    ################################################################################
+
+    data.params = Params(p)
+
+    if !vacancyEnergyCalculation
+        data.params.bandEdgeEnergy[p.iphia, p.regionIntrinsic] = p.Ea[p.regionIntrinsic]
+    end
+
+    ctsys = System(grid, data, unknown_storage = :sparse)
+
+    if test == false
+        println("*** done\n")
+    end
+    ################################################################################
+    if test == false
+        println("Define control parameters for Solver")
+    end
+    ################################################################################
+
+    control = SolverControl()
+    control.verbose = verbose
+    control.damp_initial = 0.9
+    control.damp_growth = 1.61 # >= 1
+    control.max_round = 5
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Compute solution in thermodynamic equilibrium")
+    end
+    ################################################################################
+
+    solution = equilibrium_solve!(ctsys, control = control, vacancyEnergyCalculation = vacancyEnergyCalculation)
+    inival = solution
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("I-V Measurement Loop")
+    end
+    ################################################################################
+
+    ## for saving I-V data
+    IV = zeros(0) # for IV values
+    biasValues = zeros(0) # for bias values
+
+    for istep in 2:ntsteps
+
+        t = tvalues[istep]       # Actual time
+        Δu = t * scanrate         # Applied voltage
+        Δt = t - tvalues[istep - 1] # Time step size
+
+        ## Apply new voltage (set non-equilibrium values)
+        set_contact!(ctsys, p.bregionAcceptor, Δu = Δu)
+
+        if test == false
+            println("time value: Δt = $(t)")
+        end
+
+        solution = solve(ctsys, inival = inival, control = control, tstep = Δt)
+        inival = solution
+
+        ## get I-V data
+        current = get_current_val(ctsys, solution, inival, Δt)
+
+        push!(IV, current)
+        push!(biasValues, Δu)
+
+        if plotting
+            label_solution, label_density, label_energy = set_plotting_labels(data)
+            label_solution[p.iphia] = "\$ \\varphi_a\$"
+
+            Plotter.clf()
+            plot_solution(Plotter, ctsys, solution, "bias \$\\Delta u\$ = $(Δu)", label_solution)
+            Plotter.pause(0.5)
+        end
+
+    end # time loop
+
+    ##res = [biasValues, IV]
+
+    if test == false
+        println("*** done\n")
+    end
+
+    if test == false
+        integral = integrated_density(ctsys, sol = solution, icc = p.iphia, ireg = p.regionIntrinsic)
+
+        println("Calculated average vacancy density is: ", integral / data.regionVolumes[p.regionIntrinsic])
+        println(" ")
+        if vacancyEnergyCalculation
+            vacancyEnergy = data.params.bandEdgeEnergy[p.iphia, p.regionIntrinsic] / data.constants.q
+            println("Value for vacancy energy is: ", vacancyEnergy, " eV. Save this value for later use.")
+            println("We recommend to calculate it on a fine grid.")
+            println(" ")
+        end
+    end
+
+    testval = sum(filter(!isnan, solution)) / length(solution) # when using sparse storage, we get NaN values in solution
+    return testval
+
+end # main
+
+function test()
+    testval = -0.5965842980773581; testvalvacancyEnergyCalculation = -0.5967127688772338
+    return main(test = true) ≈ testval && main(test = true, vacancyEnergyCalculation = false) ≈ testvalvacancyEnergyCalculation
+end
+
+
+end # module
---
+++ v1.2.4/module_examples/Ex202_Laser_simple.jl
@@ -0,0 +1,312 @@
+#=
+# Simple laser structure with 5 layers.
+([source code](@__SOURCE_URL__))
+
+Simulating a simple laser structure with 5 layers.
+The layers are defined by their material properties and thicknesses.
+The simulation will solve the charge transport equations across the layers,
+taking into account the stimulated recombination in the active region of the laser structure.
+=#
+
+module Ex202_Laser_simple
+
+using ChargeTransport
+using ExtendableGrids
+using GridVisualize
+using PyPlot
+
+###########################################################################
+numberOfColoumns = Dict(
+    "ref1" => [2, 4],
+    "ref2" => [4, 8],
+    "ref3" => [8, 16],
+    "ref4" => [16, 32],
+    "ref5" => [32, 64]
+)
+
+numberOfRows = Dict(
+    "ref1" => [4, 8, 2, 8, 4],
+    "ref2" => [8, 16, 4, 16, 8],
+    "ref3" => [16, 32, 8, 32, 16],
+    "ref4" => [32, 64, 16, 64, 32],
+    "ref5" => [64, 128, 32, 128, 64]
+)
+###################################################################
+
+""" Initializing X and Y coords for the tesca grid"""
+function tesca_grid(; refinement = 1, showplot = false, airbox = false)
+
+    @local_unitfactors μm
+
+    ncol = numberOfColoumns["ref$(refinement)"]
+    nrow = numberOfRows["ref$(refinement)"]
+
+    widths_columns = [1.0  10.0] * μm
+    heights_rows = [1.0  0.5  0.05  0.5  1.0] * μm
+
+    coord_x1 = collect(range(0.0, widths_columns[1], length = ncol[1] + 1))
+    coord_x2 = collect(range(widths_columns[1], sum(widths_columns[1:2]), length = ncol[2] + 1))
+    X = glue(coord_x1, coord_x2)
+    X = glue(reverse(-X), X)
+
+    coord_y1 = collect(range(0.0, heights_rows[1], length = nrow[1] + 1))
+    coord_y2 = collect(range(heights_rows[1], sum(heights_rows[1:2]), length = nrow[2] + 1))
+    coord_y3 = collect(range(sum(heights_rows[1:2]), sum(heights_rows[1:3]), length = nrow[3] + 1))
+    coord_y4 = collect(range(sum(heights_rows[1:3]), sum(heights_rows[1:4]), length = nrow[4] + 1))
+    coord_y5 = collect(range(sum(heights_rows[1:4]), sum(heights_rows[1:5]), length = nrow[5] + 1))
+    Y = glue(glue(glue(glue(coord_y1, coord_y2), coord_y3), coord_y4), coord_y5)
+
+    grid = simplexgrid(X, Y)
+
+    cellmask!(grid, [X[1], 0.0], [sum(widths_columns), heights_rows[1]], 1)
+    cellmask!(grid, [X[1], heights_rows[1]], [sum(widths_columns), sum(heights_rows[1:2])], 2)
+    cellmask!(grid, [X[1], sum(heights_rows[1:2])], [sum(widths_columns), sum(heights_rows[1:3])], 3)
+    cellmask!(grid, [X[1], sum(heights_rows[1:3])], [sum(widths_columns), sum(heights_rows[1:4])], 4)
+    cellmask!(grid, [X[1], sum(heights_rows[1:4])], [sum(widths_columns), sum(heights_rows[1:5])], 5)
+
+    # AIR
+    cellmask!(grid, [widths_columns[1], sum(heights_rows[1:4])], [sum(widths_columns), sum(heights_rows[1:5])], 6)
+    cellmask!(grid, [X[1], sum(heights_rows[1:4])], [-widths_columns[1], sum(heights_rows[1:5])], 6)
+
+    bregionDonor1 = 1      # bottom boundary
+    bregionAcceptor2 = 2   # top boundary
+    bregionNoFlux = 3
+    bregionAirBox = 4
+    bfacemask!(grid, [-widths_columns[1], sum(heights_rows)], [widths_columns[1], sum(heights_rows)], bregionAcceptor2)
+    bfacemask!(grid, [X[1], 0.0], [X[end], 0.0], bregionDonor1)
+
+    bfacemask!(grid, [X[1], 0.0], [X[1], sum(heights_rows[1:4])], bregionNoFlux)
+    bfacemask!(grid, [X[1], sum(heights_rows[1:4])], [-widths_columns[1], sum(heights_rows[1:4])], bregionNoFlux)
+    bfacemask!(grid, [-widths_columns[1], sum(heights_rows[1:4])], [-widths_columns[1], sum(heights_rows)], bregionNoFlux)
+
+    bfacemask!(grid, [X[end], 0.0], [X[end], sum(heights_rows[1:4])], bregionNoFlux)
+    bfacemask!(grid, [widths_columns[1], sum(heights_rows[1:4])], [X[end], sum(heights_rows[1:4])], bregionNoFlux)
+    bfacemask!(grid, [widths_columns[1], sum(heights_rows[1:4])], [widths_columns[1], sum(heights_rows)], bregionNoFlux)
+
+    bfacemask!(grid, [X[1], sum(heights_rows[1:4])], [X[1], Y[end]], bregionAirBox)
+    bfacemask!(grid, [X[1], Y[end]], [-widths_columns[1], Y[end]], bregionAirBox)
+    bfacemask!(grid, [widths_columns[1], Y[end]], [X[end], Y[end]], bregionAirBox)
+    bfacemask!(grid, [X[end], sum(heights_rows[1:4])], [X[end], Y[end]], bregionAirBox)
+
+    if airbox == false
+        grid = subgrid(grid, [1, 2, 3, 4, 5])
+    end
+
+    if showplot == true
+        GridVisualize.gridplot(
+            grid, Plotter = PyPlot, linewidth = 1, fontsize = 35, size = (1200, 900),
+            legend = :best, show = true, aspect = 4, colorbar = false, title = "Device Geometry, values in [m]", xlabel = "x-coordinates", ylabel = "y-coordinates"
+        )
+    end
+
+    return grid
+end
+
+function main(;
+        refinement = 1, plotting = false, verbose = false, test = false, unknown_storage = :sparse,
+        numberOfEigenvalues = 1,
+        parameter_set = Params_Laser_simple # choose the parameter set
+    )
+
+    # parameter
+    p = parameter_set()
+
+    ################################################################################
+    if test == false
+        println("Set up grid.")
+    end
+    ################################################################################
+
+    grid = tesca_grid(refinement = refinement, showplot = plotting, airbox = false)
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define System and fill in information about model")
+    end
+    ################################################################################
+
+    ## Initialize Data instance and fill in data
+    data = Data(grid, p.numberOfCarriers, numberOfEigenvalues = numberOfEigenvalues)
+
+    ## Possible choices: Stationary, Transient
+    data.modelType = Stationary
+
+    ## Possible choices: Boltzmann, FermiDiracOneHalfBednarczyk, FermiDiracOneHalfTeSCA,
+    ## FermiDiracMinusOne, Blakemore.
+    ## Can be a vector with different statistics (for n and p).
+    data.F .= FermiDiracOneHalfTeSCA
+
+    data.bulkRecombination = set_bulk_recombination(;
+        iphin = p.iphin, iphip = p.iphip,
+        bulk_recomb_Auger = true,
+        bulk_recomb_radiative = true,
+        bulk_recomb_SRH = true
+    )
+
+    data.boundaryType[p.bregionAcceptor2] = OhmicContact  # top boundary Dirichlet condition
+    data.boundaryType[p.bregionDonor1] = OhmicContact     # bottom boundary Dirichlet condition
+    #                                                     # rest is set to Neumann by default
+
+    data.fluxApproximation .= ExcessChemicalPotential
+    #data.fluxApproximation .= ScharfetterGummel          # if F=Boltzmann
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define Params and fill in physical parameters")
+    end
+    ################################################################################
+
+    """ Data from Params_Laser_simple.jl: temperature T, band edge energies EC, EV, effective density of states NC, NV
+        mobilities μn, μp, dielectricConstant εs, radiative recombination r0, life times τn, τp,
+        Auger recombination coefficients Auger_Cn, Auger_Cp
+        doping doping (or vcat(Nd,Na) = doping).
+    """
+    paramsoptical = ParamsOptical(grid, p.numberOfCarriers, numberOfEigenvalues)
+    paramsoptical.laserWavelength = p.λ
+
+    paramsoptical.absorption_0[:] = p.α0
+    paramsoptical.gain_0[:] = p.gain0
+    paramsoptical.refractiveIndex_0[:] = p.nTilde
+    paramsoptical.refractiveIndex_d[:] = p.nTilde_d
+    paramsoptical.refractiveIndex_γ[:] = p.γn
+    paramsoptical.absorptionFreeCarriers[p.iphin, :] = p.fcnalf
+    paramsoptical.absorptionFreeCarriers[p.iphip, :] = p.fcpalf
+
+    paramsoptical.eigenvalues .= 1 + 1 * im   # dummy value for initializing
+
+    data.params = Params(p)
+    data.paramsoptical = paramsoptical
+
+    ctsys = System(grid, data, unknown_storage = unknown_storage)
+
+    if test == false
+        show_params(ctsys)
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Define control parameters for Solver")
+    end
+    ################################################################################
+
+    control = SolverControl()
+    control.verbose = verbose
+    control.maxiters = 300
+    control.abstol = 1.0e-7
+    control.reltol = 1.0e-7
+    control.tol_round = 1.0e-7
+    control.max_round = 3
+    control.damp_initial = 0.8   # < 1
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Compute solution in thermodynamic equilibrium for Boltzmann")
+    end
+    ################################################################################
+
+    ## calculate equilibrium solution and set as initial guess
+    psi0Vector = electroNeutralSolution(ctsys)
+
+    inival = unknowns(ctsys)
+    inival[1, :] = inival[2, :] .= 0.0
+    inival[3, :] = psi0Vector
+
+    solution = equilibrium_solve!(ctsys, inival = inival, control = control, nonlinear_steps = 20.0)
+    inival = solution
+
+    if test == false
+        println("*** done\n")
+    end
+
+    ################################################################################
+    if test == false
+        println("Bias loop")
+    end
+    ################################################################################
+
+    maxBias = p.U[end]  # = 1.81 = topVoltageAcceptor2 # bias goes until the given voltage at acceptor boundary
+    biasValues = range(0, stop = maxBias, length = 40)
+
+    for Δu in biasValues
+
+        if test == false
+            println("bias value: Δu = ", Δu, " V")
+        end
+
+        ## set non equilibrium boundary conditions
+        set_contact!(ctsys, p.bregionAcceptor2, Δu = Δu)
+
+        solution = solve(ctsys; inival = inival, control = control)
+        inival .= solution
+
+    end # bias loop
+
+    if test == false
+        println("*** done\n")
+    end
+
+    #########################################################
+
+    ctsys.data.paramsoptical.oldSolution = solution
+    currentSolution = solution
+    inival = solution
+
+    ############################
+    ctsys.data.paramsoptical.eigenvalues = p.λ1
+    ctsys.data.paramsoptical.eigenvectors = reshape(p.v1, length(p.v1), 1)  # reshaping because in system it must be a 2D array
+    ctsys.data.paramsoptical.power = p.P[2]
+
+    solution = solve(ctsys; inival = inival, control = control)
+    ctsys.data.paramsoptical.oldSolution = solution
+    currentSolution = solution
+    inival = solution
+
+    if plotting
+        vis = GridVisualizer(; Plotter = PyPlot, fignumber = 2, resolution = (1200, 900))
+
+        scalarplot!(
+            vis, grid, solution[1, :], Plotter = PyPlot, legend = :best, clear = false, title = "Applied voltage Δu = $maxBias V",
+            xlabel = "cross section space along \$x=0\$ [m]", ylabel = "potentials [V]", fontsize = 55, linewidth = 5,
+            slice = :x => 0, label = "\$ \\varphi_n \$", color = "blue"
+        )
+
+        scalarplot!(
+            vis, grid, solution[2, :], Plotter = PyPlot, linewidth = 5,
+            slice = :x => 0, label = "\$ \\varphi_p \$", clear = false, color = "mediumvioletred"
+        )
+
+        scalarplot!(
+            vis, grid, solution[3, :], Plotter = PyPlot, linewidth = 5,
+            slice = :x => 0, label = "\$ \\psi \$", clear = false, color = "darkorange"
+        )
+    end
+
+
+    testval = sum(solution) / length(solution)
+    return testval
+
+end # main
+
+function test()
+    testval = 0.5122451923673309
+    return main(test = true) ≈ testval
+end
+
+if test == false
+    println("This message should show when this module is successfully recompiled.")
+end
+
+end # module
