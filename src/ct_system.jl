5dcb105bd3729ff464b622a7727e315da08d2090
odonovan <odonovan@wias-berlin.de>
2026-02-11 10:42:09 +0100

    Remove doubled definition of trapDistributionWidth

--- src/ct_system.jl
+++ src/ct_system.jl
@@ -449,10 +449,6 @@ mutable struct Params
     for each carrier ``\\alpha`` on each region.
     """
     mobility::Array{Float64, 2}
-    """
-    A 2D array with the corresponding trap distribution width for each trap species and region.
-    """
-    trapDistributionWidth::Array{Float64, 2}
     
 
 
@@ -592,7 +588,6 @@ function Params(numberOfRegions, numberOfBoundaryRegions, numberOfCarriers)
     params.densityOfStates = ones(Float64, numberOfCarriers, numberOfRegions)
     params.bandEdgeEnergy = zeros(Float64, numberOfCarriers, numberOfRegions)
     params.mobility = ones(Float64, numberOfCarriers, numberOfRegions)
-    params.trapDistributionWidth = zeros(Float64, numberOfCarriers, numberOfRegions)
 
     ###############################################################
     #### 2 x number of regions (for electrons and holes only!) ####
