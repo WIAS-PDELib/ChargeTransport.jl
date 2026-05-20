9c819d80d6dae1fa92801b8cb3c7f767b82cde4d
odonovan <odonovan@wias-berlin.de>
2026-05-20 14:23:24 +0200

    Remove repeated deps in test_units.jl

--- test/test_units.jl
+++ test/test_units.jl
@@ -1,8 +1,5 @@
-using Test
-using ChargeTransport
-
 @testset "Distribution functions" begin
-    
+
     @test Boltzmann(0.1) ≈ 1.1051709180756477
     @test Blakemore(0.1) ≈ 0.8511816057678475
     @test FermiDiracMinusOne(0.1) ≈ 0.52497918747894
@@ -10,5 +7,5 @@ using ChargeTransport
     @test FermiDiracOneHalfTeSCA(0.1) ≈ 0.833056882078161
     @test GaussFermiPaasch(2.0)(0.1) ≈ 0.5148909449738751
     @test GaussFermiSimpson13(2.0, 0.0, 1.0, 10_000)(0.1) ≈ 0.5151388501936096
-    
+
 end
